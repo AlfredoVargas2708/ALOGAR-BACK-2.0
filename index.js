d05afbd5ce45d04d14c1a390836a2fb82b591ae1
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-06-03 14:06:09 -0400

    refactor: remove serverless-http package and related files
    - Deleted the entire serverless-http package, including all its provider-specific implementations (AWS and Azure).
    - Removed associated utility files for request and response handling.
    - Cleaned up package.json and package-lock.json to eliminate dependencies on serverless-http.
    - Removed vercel.json configuration file.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const serverless = require('serverless-http');
 const cors = require('cors');
-const routeLoggerMiddleware = require('../middleware')
+const routeLoggerMiddleware = require('./middleware')
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const routes = require('../routes/index');
+const routes = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(routeLoggerMiddleware);
 app.use('/api', routes);
 
-module.exports = serverless(app);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
